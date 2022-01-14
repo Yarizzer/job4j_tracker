e6362c7adf1f58349f1cb179d588ca1d07985ec4
Yarizzer <yarizzer@gmail.com>
2022-01-14 10:24:54 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #250294] fixed

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -7,60 +7,40 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        Point sut = new Point(x1, y1);
-        double out = sut.distance(new Point(x2, y2));
+        Point sut = new Point(0, 0);
+        double out = sut.distance(new Point(2, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when01to00then1() {
         double expected = 1;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 0;
-        int y2 = 0;
-        Point sut = new Point(x1, y1);
-        double out = sut.distance(new Point(x2, y2));
+        Point sut = new Point(0, 1);
+        double out = sut.distance(new Point(0, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when31to10then2p23() {
         double expected = 2.23;
-        int x1 = 3;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 0;
-        Point sut = new Point(x1, y1);
-        double out = sut.distance(new Point(x2, y2));
+        Point sut = new Point(3, 1);
+        double out = sut.distance(new Point(1, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when02to20then2p82() {
         double expected = 2.82;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 2;
-        int y2 = 0;
-        Point sut = new Point(x1, y1);
-        double out = sut.distance(new Point(x2, y2));
+        Point sut = new Point(0, 2);
+        double out = sut.distance(new Point(2, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when01to22then2p23() {
         double expected = 2.23;
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 2;
-        Point sut = new Point(x1, y1);
-        double out = sut.distance(new Point(x2, y2));
+        Point sut = new Point(0, 1);
+        double out = sut.distance(new Point(2, 2));
         Assert.assertEquals(expected, out, 0.01);
     }
 }
