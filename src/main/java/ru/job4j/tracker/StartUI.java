5c23dfcf114debd24d0703a835a1fbc4af81c421
Yarizzer <yarizzer@gmail.com>
2022-01-15 14:43:09 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,10 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item(0, "name");
+        Item item1 = new Item(0, "name");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        System.out.println(item1.getCreated().format(formatter));
+        Item item2 = new Item(1, "name");
+        System.out.println(item2.toString());
     }
 }
