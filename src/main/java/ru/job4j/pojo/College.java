4bd879c7e5da91c69575e07e0812d59727b30d4f
Yarizzer <yarizzer@gmail.com>
2022-01-15 06:34:46 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setFullName("Carabas Barabas Barabasovich");
+        st.setGroup(0);
+        st.setEnrollmentDate(new Date());
+        System.out.println(st.getFullName() + " " + st.getGroup() + " " +st.getEnrollmentDate());
+    }
+}
