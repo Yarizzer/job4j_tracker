331e10a8ce30f3224d6991e71c1b67daa040cac9
Yarizzer <yarizzer@gmail.com>
2022-02-10 08:08:12 +0300

    3. Модели данных и HashSet. [#218530 #262589] fixed

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -10,11 +10,26 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class NotifyAccountTest {
-
     @Test
     public void sent() {
         List<Account> accounts = Arrays.asList(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void whenSameKeySentUnique() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("123", "Petr Arsentev", "000001"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
