35f110a51268bf4b18d42efd8324a8deeea25302
Yarizzer <yarizzer@gmail.com>
2022-03-09 15:44:55 +0300

    0. Stream API [#58166]

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class FilterNegativeNumbers {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(13, 5 -3, 8, 13);
+        List<Integer> numbers = Arrays.asList(13, 5, -3, 8, 13);
         List<Integer> positive = numbers.stream().filter(v -> v > 0).collect(Collectors.toList());
         positive.forEach(System.out::println);
     }
